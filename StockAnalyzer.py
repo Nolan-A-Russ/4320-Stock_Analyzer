a6e0178d86dab00fcfb1b9d34da5d63747501af4
Nolan-A-Russ <97975640+Nolan-A-Russ@users.noreply.github.com>
2024-03-13 09:48:04 -0500

    Type of Chart user input StockAnalyzer.py

--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -8,11 +8,18 @@ import webbrowser
 
 #Getting Stock Symbol
 
-stock_symbol = input("Enter the stock symbol: ")
+stk_symbl = input("Enter the stock symbol: ")
 
-#Get Chart Type
+#Getting Chart Type
+while True;
+  print("\n----------Chart Type----------\n------------------\n 1) Bar\n 2) Line\n")
+  chrt_type = input("1) Bar chart\n2) Line chart")
+  if chrt_type in ["1", "2"]:
+      break
+  else:
+    print("\nInvalid Input\n")
 
-#Get Time Series
+#Getting Time Series
 
 # Start Date
 
