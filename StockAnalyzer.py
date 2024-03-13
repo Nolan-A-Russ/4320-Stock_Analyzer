9582edceff1fdca9dc51759e8477bb082cb12999
Nolan-A-Russ <97975640+Nolan-A-Russ@users.noreply.github.com>
2024-03-13 10:26:35 -0500

    Update StockAnalyzer.py

--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -4,8 +4,6 @@ from lxml import html
 import pygal
 import webbrowser
 
-#Get the user information 
-
 #Getting Stock Symbol
 
 stk_symbl = input("Enter the stock symbol: ")
@@ -13,15 +11,28 @@ stk_symbl = input("Enter the stock symbol: ")
 #Getting Chart Type
 while True;
   print("\n----------Chart Type----------\n------------------\n 1) Bar\n 2) Line\n")
-  chrt_type = input("1) Bar chart\n2) Line chart")
+  chrt_type = input("Enter Chart Type 1) Bar chart\n2) Line chart")
   if chrt_type in ["1", "2"]:
       break
   else:
     print("\nInvalid Input\n")
 
 #Getting Time Series
+while True:
+    print("\n-----------Time Series-----------\n ----------------------\n 1) Intraday\n 2) Daily\n 3) Weekly\n 4) Monthly\n")
+    usr_time_series = input("Enter time series:\n 1\n 2\n 3\n 4\n Selection: ")
+    if usr_time_series in ["1","2","3","4"]:
+        break
+    else: 
+        print("\nInvalid Input\n")
+if usr_time_series == "1":
 
+if usr_time_series == "2":
+
+if usr_time_series == "3":
+
+if usr_time_series == "4":
+  
 # Start Date
-
 # End Date
 
