843a4ab2bf2f827f34e00d5885996e21cc83ee67
Nolan-A-Russ <97975640+Nolan-A-Russ@users.noreply.github.com>
2024-03-13 09:31:26 -0500

    Added stock Symbol Search StockAnalyzer.py

--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -3,3 +3,18 @@ import requests
 from lxml import html
 import pygal
 import webbrowser
+
+#Get the user information 
+
+#Getting Stock Symbol
+
+stock_symbol = input("Enter the stock symbol: ")
+
+#Get Chart Type
+
+#Get Time Series
+
+# Start Date
+
+# End Date
+
