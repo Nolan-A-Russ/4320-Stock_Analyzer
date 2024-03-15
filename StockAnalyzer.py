e6a00e13bbe695e6e7cd421b8cf1884966edfe01
Nolan-A-Russ <97975640+Nolan-A-Russ@users.noreply.github.com>
2024-03-14 19:40:59 -0500

    Finishing up Graph making code StockAnalyzer.py

--- StockAnalyzer.py
+++ StockAnalyzer.py
@@ -4,10 +4,15 @@ import webbrowser
 from lxml import html
 from datetime import datetime
 
-
+#API Info
+api_key = "QGB4RG9L7AWT1713"
+url = f'https://www.alphavantage.co/query?function={T_series}&symbol={stk_symbl}&apikey={api_key}'
+response = requests.get(url)
+data = response.json()
+tree = html.fromstring(response.text)
+print(data)
 
 #Getting Stock Symbol
-
 stk_symbl = input("Enter the stock symbol: ")
 
 #Getting Chart Type
@@ -22,24 +27,25 @@ while True;
 #Getting Time Series
 while True:
     print("\n-----------Time Series-----------\n ----------------------\n 1) Intraday\n 2) Daily\n 3) Weekly\n 4) Monthly\n")
-    usr_time_series = input("Enter time series:\n 1\n 2\n 3\n 4\n Selection: ")
-    if usr_time_series in ["1","2","3","4"]:
+    usr_T_series = input("Enter time series:\n 1\n 2\n 3\n 4\n Selection: ")
+    if usr_T_series in ["1","2","3","4"]:
         break
     else: 
         print("\nInvalid Input\n")
 
-if usr_time_series == "1":
+if usr_T_series == "1":
     T_series = "Time_Series_Intraday"
     T_series_output = "Time Series (5min)"
-if usr_time_series == "2":
+if usr_T_series == "2":
     T_series = "Time_Series_Daily"
     T_series_output = "Time Series (Daily)"
-if usr_time_series == "3":
+if usr_T_series == "3":
     T_series = "Time_Series_Weekly"
     T_series_output = "Weekly Time Series"
-if usr_time_series == "4":
+if usr_T_series == "4":
     T_series = "Time_Series_Monthly"
     T_series_output = "Monthly Time Series"
+  
 # Start Date
 while True:
     start_date = input("\nEnter Start Date (YYYY-MM-DD): ")
@@ -47,7 +53,7 @@ while True:
         datetime.strptime(start_date, '%Y-%m-%d')
         break
     except ValueError:
-          print("\nInvalid date format. Please use YYYY-MM-DD format.")
+          print("\nInvalid format. Please use YYYY-MM-DD format.")
         
 # End Date
 while True:
@@ -59,17 +65,13 @@ while True:
             else:
                 print("The end date shound't be before the start date")
         except ValueError:
-            print("\nInvalid date format. Please use YYYY-MM-DD format")
+            print("\nInvalid format. Please use YYYY-MM-DD format")
 
 print(stk_symbl, chrt_type, T_series, start_date, end_date)
 
-api_key = "QGB4RG9L7AWT1713"
-
-url = f'https://www.alphavantage.co/query?function={T_series}&symbol={stk_symbl}&apikey={api_key}'
-
-response = requests.get(url)
-data = response.json()
-print(data)
+closing_prices = []
+for date, values in data[T_series_output].items():
+    closing_prices.append(float(values['4. close']))
 
 #Creating Line Graph
 if chrt_type == 1:
@@ -79,8 +81,13 @@ if chrt_type == 1:
     chart.add('Closing Price', [float(price) for price in closing_prices])
     chart.render_to_file('stock_chart.svg')
     webbrowser.open('stock_chart.svg')
+
 #Creating Bar Graph
 if chrt_type == 2:  
     bar_chart = pygal.Bar()
     bar_chart.title = f'{stk_symbl} Stock Prices'
+    chart.x_labels = reversed([str(i) for i in range(1, len(closing_prices) + 1)])
+    chart.add('Closing Price', [float(price) for price in closing_prices])
+    chart.render_to_file('stock_chart.svg')
+    webbrowser.open('stock_chart.svg')
 
